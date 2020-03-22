7ca86bd6dbac1fc111a91b72530c42576ca69678
Bryn Mailer <bryndiupero@gmail.com>
2020-03-22 16:22:12 +1000

    feat(Settings): settings page
    Add settings page allowing users to manage authorized Atlassian sites.
    As well as set their preferred currency display format.

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,108 +1,225 @@
-import React        from 'react';
+import React              from 'react';
+
+/* Material Table */
+import MaterialTable      from 'material-table';
 
 /* Material UI */
 import {
   makeStyles
-}                   from '@material-ui/core/styles';
+}                         from '@material-ui/core/styles';
 import {
-  Paper,
-  Typography,
-  Grid
-}                   from '@material-ui/core';
+  Card,
+  CardHeader,
+  CardContent,
+  Grid,
+  TextField,
+  IconButton
+}                         from '@material-ui/core';
+import SaveIcon            from '@material-ui/icons/Save';
+
+/* Formik */
+import {
+  Formik,
+  Form,
+  Field
+}                         from 'formik';
+
+/* Formik Material UI */
+import {
+  TextField as FTextField
+}                         from 'formik-material-ui';
+
+/* Yup */
+import * as Yup           from 'yup';
+
+/* Currency.JS */
+import currency           from 'currency.js';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3),
-    height: '100%'
+  topMargin: {
+    marginTop: theme.spacing(2)
   }
 }));
 
+const CurrencyFormatSchema = Yup.object().shape({
+  code: Yup.string()
+    .min(3, 'Code is too short')
+    .max(3, 'Code is too long')
+    .required('Required'),
+  precision: Yup.number()
+    .positive('Precision must be positive')
+    .min(1, 'Precision is too low')
+    .max(5, 'Precision is too high')
+    .required('Required'),
+  thousand: Yup.string()
+    .max(2, 'Separator is too long')
+    .required('Required'),
+  decimal: Yup.string()
+    .max(2, 'Decimal is too long')
+    .required('Required')
+});
+
 const Settings = ({ authorized }) => {
+  const [ sites, setSites ] = React.useState({
+    columns: [
+      { title: 'Domain', field: 'domain' }
+    ],
+    data: [
+      { domain: 'example.atlassian.net' }
+    ]
+  });
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Typography
-        variant="h3"
-        align="center"
-      >
-        Overview
-      </Typography>
-      <Grid
-        className={classes.grid}
-        container
-        direction="column"
-        justify="space-around"
-        alignItems="stretch"
-      >
-        <Grid
-          item
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <Typography
-            component={Grid}
-            variant="h4"
-            color="primary"
-            align="center"
-            item
+    <>
+      <MaterialTable
+        title="Managed Atlassian Sites"
+        components={{
+          Container: Card
+        }}
+        options={{
+          search: false,
+          pageSize: 3,
+          pageSizeOptions: [ 3 ],
+          actionsColumnIndex: -1
+        }}
+        columns={sites.columns}
+        data={sites.data}
+        actions={[
+          {
+            icon: 'add',
+            tooltip: 'Add Site',
+            isFreeAction: true,
+            onClick: () => {
+              setSites(prevSites => {
+                const data = [ ...prevSites.data ];
+                data.push({ domain: 'example.atlassian.net' });
+                return { ...prevSites, data };
+              });
+            }
+          },
+          rowData => ({
+            icon: 'delete',
+            tooltip: 'Forget Site',
+            onClick: (event, rowData) => {
+              setSites(prevSites => {
+                const data = [ ...prevSites.data ];
+                data.splice(data.indexOf(rowData), 1);
+                return { ...prevSites, data };
+              });
+            }
+          })
+        ]}
+      />
+      <Card className={classes.topMargin}>
+        <CardHeader
+          title="Currency Format"
+          titleTypographyProps={{
+            variant: 'h6'
+          }}
+          action={
+            <IconButton>
+              <SaveIcon />
+            </IconButton>
+          }
+        />
+        <CardContent>
+          <Formik
+            initialValues={{
+              code: 'AUD',
+              precision: 2,
+              thousand: ',',
+              decimal: '.'
+            }}
+            validationSchema={CurrencyFormatSchema}
           >
-            83 hours 15 mins
-          </Typography>
-          <Typography
-            component={Grid}
-            variant="h5"
-            align="center"
-            item
-          >
-            worked this month
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <Typography
-            component={Grid}
-            variant="h4"
-            color="primary"
-            align="center"
-            item
-          >
-            Invoices:
-          </Typography>
-          <Typography
-            component={Grid}
-            variant="h5"
-            align="center"
-            item
-          >
-            0 Overdue
-          </Typography>
-          <Typography
-            component={Grid}
-            variant="h5"
-            align="center"
-            item
-          >
-            0 Pending
-          </Typography>
-          <Typography
-            component={Grid}
-            variant="h5"
-            align="center"
-            item
-          >
-            23 Fulfilled
-          </Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+            {({ values }) => (
+              <Grid
+                component={Form}
+                container
+                justify="space-around"
+                spacing={2}
+              >
+                <Grid
+                  item
+                  xs={6}
+                >
+                  <Field
+                    component={FTextField}
+                    label="Currency Code"
+                    name="code"
+                    type="text"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                >
+                  <Field
+                    component={FTextField}
+                    label="Precision"
+                    name="precision"
+                    type="number"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                >
+                  <Field
+                    component={FTextField}
+                    label="Thousand Separator"
+                    name="thousand"
+                    type="text"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                >
+                  <Field
+                    component={FTextField}
+                    label="Decimal"
+                    name="decimal"
+                    type="text"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                >
+                  <TextField
+                    label="Preview"
+                    type="text"
+                    variant="outlined"
+                    InputProps={{
+                      readOnly: true
+                    }}
+                    value={
+                      currency(
+                        1000,
+                        {
+                          formatWithSymbol: true,
+                          pattern: '# !',
+                          symbol: values.code,
+                          precision: values.precision,
+                          separator: values.thousand,
+                          decimal: values.decimal
+                        }
+                      ).format()
+                    }
+                  />
+                </Grid>
+              </Grid>
+            )}
+          </Formik>
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
