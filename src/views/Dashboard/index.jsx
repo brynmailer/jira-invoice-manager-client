ba05d5381d5d80a7b33c912c94045da24cf9383b
Bryn Mailer <bryn@trustprovenance.com>
2020-06-02 12:17:44 +1000

    fix(Dashboard): result of check for user refresh token no longer cached

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 /* GraphQL */
 import { gql } from "apollo-boost";
@@ -8,23 +8,35 @@ import { useQuery } from "@apollo/react-hooks";
 import { AtlassianPopup, Overview } from "./components";
 import { Page } from "../../components";
 
-const GET_ACCESSIBLE_RESOURCES = gql`
-  query GetAccessibleResources {
-    getAccessibleResources {
-      id
+const GET_USER_REFRESH_TOKEN = gql`
+  query GetUserRefreshToken {
+    currentUser {
+      refreshToken
     }
   }
 `;
 
 const Dashboard = () => {
-  const { data, loading, error } = useQuery(GET_ACCESSIBLE_RESOURCES);
+  const [authorized, setAuthorized] = useState(false);
+  const { data, loading, error } = useQuery(GET_USER_REFRESH_TOKEN, {
+    fetchPolicy: "no-cache",
+  });
+
+  useEffect(() => {
+    let mounted = true;
+    if (!error && !loading && mounted)
+      setAuthorized(data.currentUser.refreshToken ? true : false);
+
+    return () => (mounted = false);
+  }, [data, loading, error]);
 
   if (loading) {
     return <Page loading />;
   } else {
     return (
       <Page navbar>
-        <Overview />
+        <AtlassianPopup authorized={authorized} />
+        <Overview authorized={authorized} />
       </Page>
     );
   }
