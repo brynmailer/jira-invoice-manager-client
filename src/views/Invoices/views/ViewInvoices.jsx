5ca92c9f45af1185fd148e3f0be2e4bedd1beddf
Bryn Mailer <bryndiupero@gmail.com>
2020-06-20 14:32:40 +1000

    feat(Invoices): get invoices from API

--- src/views/Invoices/views/ViewInvoices.jsx
+++ src/views/Invoices/views/ViewInvoices.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+/* GraphQL */
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
 /* Material Table */
 import MaterialTable from "material-table";
 
@@ -9,75 +13,64 @@ import { useHistory } from "react-router-dom";
 /* Components */
 import { Page } from "../../../components";
 
-const ViewInvoices = () => {
-  const history = useHistory();
+const GET_INVOICES = gql`
+  query GetInvoices {
+    currentUser {
+      invoices {
+        id
+        billTo
+        number
+        status
+      }
+    }
+  }
+`;
 
-  return (
-    <Page navbar>
-      <MaterialTable
-        columns={[
-          { title: "Bill To", field: "billTo" },
-          { title: "Total", field: "total" },
-          { title: "Status", field: "status" },
-        ]}
-        data={[
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "paid",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "overdue",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "issued",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "issued",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "paid",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "overdue",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "overdue",
-          },
-          {
-            billTo: "Test Company",
-            total: "$5000",
-            status: "paid",
-          },
-        ]}
-        actions={[
-          {
-            icon: "add",
-            tooltip: "Create Invoice",
-            isFreeAction: true,
-            onClick: () => history.push("/invoices/create"),
-          },
-        ]}
-        options={{
-          showTitle: false,
-          pageSize: 10,
-          pageSizeOptions: [5, 10, 20],
-        }}
-      />
-    </Page>
-  );
+const ViewInvoices = () => {
+  const { data, loading, error } = useQuery(GET_INVOICES);
+  const history = useHistory();
+  const priorities = {
+    overdue: 1,
+    pending: 2,
+    paid: 3,
+  };
+
+  if (loading || error) {
+    return <Page loading />;
+  } else {
+    return (
+      <Page navbar>
+        <MaterialTable
+          columns={[
+            { title: "Bill To", field: "billTo" },
+            { title: "Number", field: "number" },
+            {
+              title: "Status",
+              field: "status",
+              customSort: (a, b) => priorities[a.status] - priorities[b.status],
+              defaultSort: "asc",
+            },
+          ]}
+          data={data.currentUser.invoices}
+          actions={[
+            {
+              icon: "add",
+              tooltip: "Create Invoice",
+              isFreeAction: true,
+              onClick: () => history.push("/invoices/create"),
+            },
+          ]}
+          options={{
+            showTitle: false,
+            search: false,
+            sorting: true,
+            pageSize: 10,
+            pageSizeOptions: [10],
+          }}
+        />
+      </Page>
+    );
+  }
 };
 
 export default ViewInvoices;
